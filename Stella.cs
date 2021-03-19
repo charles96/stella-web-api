55b23750e4f0670d320ba2c507e88d1b30ec6eab
찰스.홍인철 <ichong@lunasoft.co.kr>
2021-03-19 19:50:34 +0900

    read write test

--- Stella.cs
+++ Stella.cs
@@ -12,8 +12,28 @@ namespace stella_web_api
     public static class Stella
     {
         [FunctionName("compliment")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> ComplimentWriteRunAsync(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "compliment/Write")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request..");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed succe44ssfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("compliment")]
+        public static async Task<IActionResult> ComplimentReadRunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "compliment/read")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request..");
