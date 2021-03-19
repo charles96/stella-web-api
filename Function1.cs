e82a1c717357ac95589903b698c6ec3166026906
찰스.홍인철 <ichong@lunasoft.co.kr>
2021-03-19 15:52:59 +0900

    test commit

--- Function1.cs
+++ Function1.cs
@@ -16,7 +16,7 @@ namespace stella_web_api
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request..");
 
             string name = req.Query["name"];
 
